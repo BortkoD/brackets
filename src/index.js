a19b5b5da44fa0c90baf51d0face8bf7df91093d
aria ortko <megymikei@gmail.com>
2022-02-22 22:48:59 +0300

    all tests is ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let checkStr = '';
+    for (let i = 0; i < str.length; i++) {
+        let c = 0;
+        for (; c < bracketsConfig.length; c++) {
+            if (str[i] === bracketsConfig[c][0] && (checkStr.length < i+1 || checkStr[i] != str[i])) {
+                checkStr = checkStr.slice(0, i) + bracketsConfig[c][0] + bracketsConfig[c][1] + checkStr.slice(i);
+                break;
+            }
+        } 
+        if (c == bracketsConfig.Length && str[i] != checkStr[i]) return false;
+    }
+    if (checkStr === str) return true;
+    else return false;
 }
